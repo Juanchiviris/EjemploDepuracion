fde3117eea03d4768cd8dca116b5ebe88d011fd7
Juan Jose <juanxxo130804@gmail.com>
2025-02-13 02:35:47 +0100

    Cambio fichero esPalindromo

--- src/Actividad1/esPalindromo.java
+++ src/Actividad1/esPalindromo.java
@@ -1,5 +1,6 @@
 package Actividad1;
 
+// Modificado por Juan Jose Moncaleano forero
 public class esPalindromo {
 	public static boolean validPalindrome(String s) {
 	       for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
